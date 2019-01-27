f9a47863eaae127eb438c8b94b44f3d6ee3f665b
Roman Kovalchuk <rkovalchuk@it.ru>
2019-01-28 00:34:12 +0300

    add Jump and Mine prefab

---
+++ Assets/Scripts/Mine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    [SerializeField] private float explodeTime = 2;
+    [SerializeField] private float explodeForce = 100;
+    [SerializeField] private float explodeRadius = 5;
+    [SerializeField] private float damage = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Explode ()
+    {
+        Collider2D[] enemiesColider = Physics2D.OverlapCircleAll(gameObject.transform.position, explodeRadius);
+        foreach (Collider2D enemyColider in enemiesColider) {
+            GameObject enemyGO = enemyColider.gameObject;
+            bool isEnemy = enemyGO.GetComponent<Enemy>();
+            if (isEnemy)
+            {
+                enemyGO.GetComponent<Health>().takeDamage(damage);
+                enemyGO.GetComponent<Rigidbody2D>().AddForce(Vector2.up * explodeForce, ForceMode2D.Impulse);
+            }
+        }
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Invoke("Explode", explodeTime);
+        }
+    }
+}
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -5,19 +5,61 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    //Player RG
+    private Rigidbody2D rg;
+    private bool isOnGround = false;
+    private bool isSecondJumpAvailable = false;
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        rg = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        //Player movement
         float direction = Input.GetAxis("Horizontal");
         Vector2 currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
         Vector2 movement = new Vector2(direction * speed * Time.fixedDeltaTime, currentVelocity.y);
-        gameObject.GetComponent<Rigidbody2D>().velocity = movement;
+        rg.velocity = movement;
+
+        //Player jump
+        bool jump = Input.GetButtonDown("Jump");
+        if (jump)
+        {
+            if (isOnGround)
+            {
+                Jump();
+            } else if (isSecondJumpAvailable)
+            {
+                Jump();
+                isSecondJumpAvailable = false;
+            }
+        }
+    }
+
+    private void Jump ()
+    {
+        rg.AddForce(Vector2.up * jumpForce);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Ground collision");
+        if (collision.gameObject.tag=="Ground")
+        {
+            isOnGround = true;
+            isSecondJumpAvailable = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isOnGround = false;
+        }
     }
 }
