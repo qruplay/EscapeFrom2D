0cc218a88563a20353809c61ea8bf816b74d668b
Roman Kovalchuk <rkovalchuk@it.ru>
2019-02-05 09:02:18 +0300

    mine refactoring

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -8,39 +8,29 @@ public class Mine : MonoBehaviour
     [SerializeField] private float _explodeForce = 100;
     [SerializeField] private float _explodeRadius = 5;
     [SerializeField] private float _damage = 50;
+    [SerializeField] private float _triggerRadius = 1;
+    [SerializeField] private LayerMask _enemyLayer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        Collider2D triggerEnemyColider = Physics2D.OverlapCircle(transform.position, _triggerRadius, _enemyLayer);
+
+        if (triggerEnemyColider)
+        {
+            Invoke("Explode", _explodeTime);
+        }
     }
 
     private void Explode ()
     {
-        Collider2D[] enemiesColider = Physics2D.OverlapCircleAll(gameObject.transform.position, _explodeRadius);
+        Collider2D[] enemiesColider = Physics2D.OverlapCircleAll(gameObject.transform.position, _explodeRadius, _enemyLayer);
         foreach (Collider2D enemyColider in enemiesColider) {
             GameObject enemyGO = enemyColider.gameObject;
-            bool isEnemy = enemyGO.GetComponent<Enemy>();
-            if (isEnemy)
-            {
-                enemyGO.GetComponent<Health>().takeDamage(_damage);
-                enemyGO.GetComponent<Rigidbody2D>().AddForce(Vector2.up * _explodeForce, ForceMode2D.Impulse);
-            }
-        }
-        Destroy(gameObject);
-    }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
-            Invoke("Explode", _explodeTime);
+            enemyGO.GetComponent<Health>().takeDamage(_damage);
+            enemyGO.GetComponent<Rigidbody2D>().AddForce(Vector2.up * _explodeForce, ForceMode2D.Impulse);
         }
+
+        Destroy(gameObject);
     }
 }
