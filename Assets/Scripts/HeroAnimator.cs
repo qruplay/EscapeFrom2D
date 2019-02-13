ad7aab6b74ba99f2c0a4e7fb8247adfa89bb123f
Roman Kovalchuk <rkovalchuk@it.ru>
2019-02-14 00:06:05 +0300

    added animation for hero idle and move

---
+++ Assets/Scripts/HeroAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class HeroAnimator : MonoBehaviour
+{
+
+    private Animator _myAnimController;
+    private Rigidbody2D _myRG;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myAnimController = GetComponent<Animator>();
+        _myRG = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(_myAnimController.GetFloat("Movespeed"));
+        _myAnimController.SetFloat("Movespeed", Mathf.Abs(_myRG.velocity.x));
+    }
+}
